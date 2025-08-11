0f79ee9fcc60f53628ccc3536a12f78d8d50572c
manishdevaraj <manishdevaraj03@gmail.com>
2025-08-11 21:09:29 +0530

    t3

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,23 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import ViteSitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), ViteSitemap(
+    {
+      hostname: 'https://srivenkateshtraders.in',
+      // routes: [
+      //   '/',             // Home
+      //   '/shop',          // Shop page
+      //   "/track-order",   // Track Order page
+      //   '/Aboutus',        // About page
+      //   '/Contactus',      // Contact page
+      // ],
+      outDir: 'dist'
+    }
+  )],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
